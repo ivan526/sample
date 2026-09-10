e4ec5a7fa15a26b26b0895913b6f81f824cd523b
ivan526 <48591229+ivan526@users.noreply.github.com>
2026-09-10 17:53:25 +0800

    feat: mount user Excel import

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.jsx";
+import UserExcelImportEnhancer from "./components/UserExcelImportEnhancer.jsx";
 import "./styles.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <UserExcelImportEnhancer />
   </React.StrictMode>,
 );
